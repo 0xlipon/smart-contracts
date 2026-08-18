831d3e5377477e612808dd411db4541fe21e299a
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2026-08-18 10:56:15 +0600

    Create solidity-basic-types.sol

---
+++ solidity/simple-storage/solidity-basic-types.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36; // solidity versions
+
+contract SimpleStorage {
+
+    // Basic Types: boolean, uint, int, address, bytes
+    // each line ends with semecolon (;)
+
+    // bool hasFavouriteNumber = true; // boolean
+
+    // uint an alias for uint256, other unsigned integer sizes: uint8,uint16,uint32,uint64,uint128
+    // uint hasFavouriteNumber = 88;  // default value 0
+
+    // int256 hasFavouriteNumber = -77; // integer could both positive & negative
+
+    // string hasFavouriteNumberInText = "liponsec";
+    // int256 favouriteInt = -55;
+
+    // address myAddress = 0x9E5b1a545017DBe75c233151C6a53f7C8B64036d;
+
+    // bytes32 favouriteBytes32 = "cat"  // max 32
+    
+}
