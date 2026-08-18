573ec9d107b0853cbf08e5e41121b9bb2fb12919
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2026-08-18 11:26:23 +0600

    Update solidity-functions.sol

--- solidity/simple-storage/solidity-functions.sol
+++ solidity/simple-storage/solidity-functions.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36; // solidity versions
 
+contract SimpleStorage {
+
+   uint favouriteNumber;  // default value 0 if not initialized
+
+   function store(uint256 _favouriteNumber) public{
+    favouriteNumber = _favouriteNumber; // set favouriteNumber variable to whatever _favouriteNumber variable parse
+   }
+
+}
