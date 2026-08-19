0aa968225c9dccce3058a45a9ea012ed7afa1565
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2026-08-19 20:59:23 +0600

    Update solidity-functions.sol

--- solidity/simple-storage/solidity-functions.sol
+++ solidity/simple-storage/solidity-functions.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.36; // solidity versions
 
 contract SimpleStorage {
 
-   uint favouriteNumber;  // default value 0 if not initialized
+   uint public favouriteNumber;  // default value 0 if not initialized // default visibility internal
 
    function store(uint256 _favouriteNumber) public{
     favouriteNumber = _favouriteNumber; // set favouriteNumber variable to whatever _favouriteNumber variable parse
+   }
+
+   // view - can read state, cannot modify state
+   // pure - cannot read or modify state
+   function retrive() public view returns(uint256){
+      return favouriteNumber; // view
+   }
+   function retriveme() public pure returns(uint256){
+      return 7; // pure
    }
 
 }
