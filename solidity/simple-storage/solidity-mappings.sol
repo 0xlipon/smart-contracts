dd93be48e0c41615f76e6274c22dcd004e8426a9
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2026-08-19 22:04:36 +0600

    Create solidity-mappings.sol

---
+++ solidity/simple-storage/solidity-mappings.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36;
+
+contract SimpleStorage {
+
+   uint256 myFavouriteNumber;
+
+   // Structs
+   struct Person{
+      uint256 favouriteNumber;
+      string name;
+   }
+
+   // dynamic array
+   Person[] public listOfPeople;
+
+   // mapping ex: lipon => 7 , default value is always zero
+   mapping(string => uint256) public nameToFavouriteNumber;
+
+   function store(uint256 _favouriteNumber) public{
+    myFavouriteNumber = _favouriteNumber;
+   }
+
+   // view, pure
+   function retrive() public view returns(uint256){
+      return myFavouriteNumber; // view
+   }
+
+   // calldata, memory, storage
+   function addPerson(string memory _name, uint256 _favouriteNumber) public{
+   listOfPeople.push( Person(_favouriteNumber, _name) );
+   nameToFavouriteNumber[_name] = _favouriteNumber;
+   }
+}
